551b89474c5e4bb539b92f1d0fe1a7abe72dc13a
Scott Cross <scgamer@comcast.net>
2022-12-01 22:43:16 -0500

    Testing termcolor

--- exercise_files/hello.py
+++ exercise_files/hello.py
@@ -1,3 +1,11 @@
-
+import sys
+import os
+from termcolor import colored, cprint
 # Print "hello, world!" to the terminal
+os.system('color')
+print (sys.version)
 print('Hello, World!')
+
+text = colored('Hello red inverted blinking World!', 'white', attrs=["bold"])
+print (text)
+cprint("Hello blue world", 'blue', 'on_white')
